8f181adf96bac26a82b6c6a4fa016651aea8fa08
LeonardoP448 <Leonardop448@hotmail.com>
2025-06-29 17:34:54 -0500

    cambio nombre bd

--- Modelo/Conexion.php
+++ Modelo/Conexion.php
@@ -1,33 +1,35 @@
 <?php
 //clase para la conexion
 
-class Conexion{
-	
+class Conexion
+{
+
 	//declaramos los valores
 	private $nombreHost = "localhost";
-	private $nombreDB = "bd_apuestas";
+	private $nombreDB = "pulcastc_bd_apuestas";
 	private $usuarioDB = "root";
 	private $contrasenaUsuario = "";
-   // private $nombreDB = "vaucher";
+	// private $nombreDB = "vaucher";
 	//private $usuarioDB = "root";
 	//private $contrasenaUsuario = "";
-   // 
-    
-	private $caracter =  "utf8";
-	
+	// 
+
+	private $caracter = "utf8";
+
 	//metodo estatico publico para que se conecte.
-	function conectar(){
-		
-		
-		$conexion ="mysql:host=".$this->nombreHost.";dbname=".$this->nombreDB.";
-		charset=".$this->caracter;
-		
-		$pdo = new PDO($conexion,$this->usuarioDB, $this->contrasenaUsuario);
-		
+	function conectar()
+	{
+
+
+		$conexion = "mysql:host=" . $this->nombreHost . ";dbname=" . $this->nombreDB . ";
+		charset=" . $this->caracter;
+
+		$pdo = new PDO($conexion, $this->usuarioDB, $this->contrasenaUsuario);
+
 		return $pdo;
-	
-		
+
+
 	}
-	
-	
+
+
 }
